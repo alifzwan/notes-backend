be7f279aa6738e6f12fbb37968fa887eb4fed621
alifzwan <alifzakwan529@gmail.com>
2024-05-06 11:27:08 +0800

    chore: Refactor mongo.js for better code organization and readability

--- mongo.js
+++ mongo.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose')
 
-if(process.argv.length < 3) {
+if(process.argv.length < 3) { // if the password is not provided as an argument, the program will exit.
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2] // the password is taken from the command line argument.
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.on1wn8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false) // This is to avoid the deprecation warning.
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema({ // The schema is defined for the Note model.
     content: String,
     important: Boolean,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema) // The Note model is created based on the schema.
 
-const note = new Note({
+const note = new Note({ // A new note object is created based on the model.
     content: 'HTML is Easy',
     important: true,
 })
 
-note.save().then(result => {
+note.save().then(result => { // The note object is saved to the database.
     console.log('note saved!')
     mongoose.connection.close()
 })
