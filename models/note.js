d5875a83693b270d64fb659e1aa84b3ff3600136
alifzwan <alifzakwan529@gmail.com>
2024-05-06 15:41:48 +0800

    chore: Refactor note.js schema and toJSON transformation

--- models/note.js
+++ models/note.js
@@ -13,17 +13,17 @@ mongoose.connect(url)
         console.log('Error connecting to MongoDB:', error.message)
     })
 
-    const noteSchema = new mongoose.Schema({
-        content: String,
-        important: Boolean,
-    })
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
 
-    noteSchema.set('toJSON', {
-        transform:(document, returnedObject) => {
-            returnedObject.id = returnedObject._id.toString()
-            delete returnedObject._id
-            delete returnedObject.__v
-        }
-    })
+noteSchema.set('toJSON', {
+    transform:(document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 
     module.exports = mongoose.model('Note', noteSchema)
