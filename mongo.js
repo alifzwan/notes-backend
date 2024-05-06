f7e11ee390a74411497bb085fe70f083d9bf3167
alifzwan <alifzakwan529@gmail.com>
2024-05-06 14:32:45 +0800

    feat: Save note to MongoDB with command line arguments
    The code changes in `mongo.js` modify the script to save a note to MongoDB using command line arguments for the note content and importance. This update enhances the functionality of the script by allowing users to specify the content and importance of the note when running the script.

--- mongo.js
+++ mongo.js
@@ -6,26 +6,47 @@ if(process.argv.length < 3) { // if the password is not provided as an argument,
 }
 
 const password = process.argv[2] // the password is taken from the command line argument.
+const content = process.argv[3] // the content of the note is taken from the command line argument.
+const important = process.argv[4] // the importance of the note is taken from the command line argument.
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.on1wn8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = 
+`mongodb+srv://fullstack:${password}@cluster0.on1wn8q.mongodb.net/?
+retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set("strictQuery", false) // This is to avoid the deprecation warning.
 
 mongoose.connect(url)
 
+//? Schema - The schema is defined for the Note model.
 const noteSchema = new mongoose.Schema({ // The schema is defined for the Note model.
     content: String,
     important: Boolean,
 })
 
+//? Model - The Note model is created based on the schema.
 const Note = mongoose.model('Note', noteSchema) // The Note model is created based on the schema.
 
+//? Object - A new note object is created based on the model.
 const note = new Note({ // A new note object is created based on the model.
-    content: 'HTML is Easy',
-    important: true,
+    content: content,
+    important: important,
 })
 
-note.save().then(result => { // The note object is saved to the database.
-    console.log('note saved!')
-    mongoose.connection.close()
-})
+//! Fetching objects(note) from the database
+// Note.find({}).then(result => { // The notes in the database are fetched.
+//     if (result.length > 0) { // Check if there are notes found
+//         result.forEach(note => { // The notes are logged to the console.
+//             console.log(note)
+//         })
+//     } else {
+//         console.log('No notes found!')
+//     }
+//     mongoose.connection.close() // The database connection is closed.
+// })
+
+
+//! The note object is saved to the database.
+// note.save().then(result => { // The note object is saved to the database.
+//     console.log(result, 'note saved!')
+//     mongoose.connection.close()
+// })
