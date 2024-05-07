ad06ea6e954354f70f2d9a0967068a4e750b2a42
alifzwan <alifzakwan529@gmail.com>
2024-05-07 10:23:03 +0800

    chore: Refactor code for improved organization and readability

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-require('dotenv').config() // Import the dotenv library
-const express = require('express') // Import the Express.js library
-const app = express() // Create an instance of Express application
-// const cors = require('cors') // Import cors
-const mongoose = require('mongoose') // Import mongoose
-const Note = require('./models/note') // Import the Note model
-const password = process.argv[2]
+require('dotenv').config() // The dotenv package is used to read environment variables from a .env file.
+
+const express = require('express')
+const cors = require('cors') 
+const Note = require('./models/note') 
+const app = express()
+
+
+app.use(express.static('dist')) // The express.static middleware is used to serve static files from the dist directory.
+app.use(express.json()) // The express.json middleware is used to parse JSON payloads in the request body.
+app.use(cors()) // The cors middleware is used to allow requests from all origins.
+
+// requestLogger middleware
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger) // The requestLogger middleware is used to log information about the requests that are made to the server.
 
 
 
-// app.use(express.json()) // Enable the use of JSON data in the request body
-// app.use(cors()) // Use cors to allow requests from other origins
-app.use(express.static('dist'))
 
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
-
+// GET
 app.get('/', (request, response) => { 
     response.send('<h1>Hello World!</h1>') // Send a response to the client
 })
 
+// GET
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(result => {
-        response.json(result)
+    Note.find({}).then(notes => {
+        response.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (request, response) => { 
-    const id = Number(request.params.id ) // Extract the id parameter from the request
-    const note = notes.find(note => note.id === id) // Find the note with the corresponding id
-    
-    if (note) {
-        response.json(note)        
-    }else {
-        response.status(404).end()
+// GET
+app.get('/api/notes/:id', (request, response, next) => { 
+    Note.findById(request.params.id)
+        .then(note => {
+            if(note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+// DELETE
+app.delete('/api/notes/:id', (request, response, next) => { 
+    Node.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+// PUT
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important
     }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => { 
-    const id = Number(request.params.id) // Extract the id parameter from the request
-    notes = notes.filter(note => note.id !== id) // Create a new array that does not contain the note with the specified id
 
-    response.status(204).end() // Send a 204 No Content status code
-})
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -66,26 +83,52 @@ const generateId = () => {
     return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+// POST
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content){ // If the content field is missing, 
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    } 
-
-    const note = {
-        id: generateId(),
-        content: body.content,
-        important: Boolean(body.important) || false,
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
     }
-
-    notes = notes.concat(note)
-    console.log(notes)
-    response.json(note)
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
+
+
+
+
+
+// unknownEndpoint middleware
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ 
+        error: 'unknown endpoint' 
+    })
+}
+
+// errorHandler middleware
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if( error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(unknownEndpoint) // The unknownEndpoint middleware is used to catch requests made to unknown routes.
+app.use(errorHandler) // The errorHandler middleware is used to catch errors in request handling.
 
 
 
